c6fa553750292ebf796d59151013b65869ed5228
Furkan SALMANLI <70803852+furkansalmanli@users.noreply.github.com>
2020-09-16 11:08:13 +0300

    Add files via upload

---
+++ StudentWebService/StudentWebService/Account Management/loginStatus.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace StudentWebService.Account_Management
+{
+    /// <summary>
+    ///  Giriş ekranı ve bu ekrana bağlanma kontrol fonksiyonlarını kullandık.
+    /// </summary> 
+
+    public class loginStatus
+    {
+
+        static string connectingStringAddStudent = "Data Source = TOSHIBA\\SQLEXPRESS; Initial Catalog = Student; Integrated Security = True";
+
+        /// <summary>
+        /// Parametre olarak kullanıcı adı,şifre ve ekran mesajı  alarak  kullanıcı girişi sağlar.
+        /// </summary> 
+
+        public static void loginScreen(string message, string loginId, string loginPassword)
+        {
+            SqlConnection connection = new SqlConnection(connectingStringAddStudent);
+
+            loginStatus hello = new loginStatus();
+
+
+
+
+
+
+            string kullanici = loginId;
+            string sifre = loginPassword;
+            
+            SqlCommand komut = new SqlCommand("SELECT * FROM  login WHERE user=@user AND password=@password", connection);
+            komut.Parameters.AddWithValue("@user", kullanici);
+            komut.Parameters.AddWithValue("@password", sifre);
+            connection.Open();
+            SqlDataReader oku = komut.ExecuteReader();
+            if (oku.Read())
+            {
+                //Session["Kullanici"] = oku["user"].ToString();
+                HttpContext.Current.Response.Redirect("HomePage.aspx");
+            }
+            else
+                message = "Kullanıcı adı ya da şifre hatalı!";
+            oku.Close();
+            connection.Close();
+            connection.Dispose();
+
+
+        }
+        /// <summary>
+        /// Parametre olarak kullanıcı adı,şifre ve ekran mesajı  alarak  kullanıcı girişinin doğru olup olmadığını session ile kontrol eder ve yönlendirme yapar.
+        /// </summary> 
+        public static void loginConnect(string message, string loginId, string loginPassword)
+        {
+
+
+            //    if (Session["Kullanici"] == null)
+            HttpContext.Current.Response.Redirect("HomePage.aspx");
+            //else
+          //      HttpContext.Current. Response.Write("Hoşgeldiniz " + Session["Kullanici"].ToString());
+        }
+    }
+
+      
+}
---
+++ StudentWebService/StudentWebService/Note Management/noteStatus.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace StudentWebService.Note_Management
+{
+    /// <summary>
+    /// Öğrenci notu ekleme, silme ve listeleme  fonksiyonları kullandık.
+    /// </summary> 
+    public class noteStatus
+    {
+
+        static string connectingStringAddStudent = "Data Source = TOSHIBA\\SQLEXPRESS; Initial Catalog = Student; Integrated Security = True";
+
+        /// <summary>
+        /// Parametre olarak öğrenci numarası,vize notu,final notu,sonuç notu alarak öğrenci notu ekleme işlemi yapar.
+        /// </summary> 
+        public static void addNote(string ogrNo, string vize, string final, string sonuc)
+        {
+
+            SqlConnection connection = new SqlConnection(connectingStringAddStudent);
+            connection.Open();
+            SqlCommand komut = new SqlCommand("INSERT INTO exams (stdNo, midterm, final , result ) VALUES(  @ogrNo, @vize, @final, @sonuc)", connection);
+
+            komut.Parameters.Add(new SqlParameter("@ogrNo", ogrNo));
+
+            komut.Parameters.Add(new SqlParameter("@vize", vize));
+
+            komut.Parameters.Add(new SqlParameter("@final", final));
+
+            komut.Parameters.Add(new SqlParameter("@sonuc", sonuc));
+            //  komut.Parameters.Add("@sonuc", System.Data.SqlDbType.NVarChar, 255);
+
+            komut.ExecuteNonQuery();
+
+
+
+            connection.Close();
+        }
+
+        /// <summary>
+        /// Parametre olarak öğrenci numarası alarak öğrenci notu silme işlemi yapar.
+        /// </summary>     
+        public static void deleteNote(string ogrNo)
+        {
+            SqlConnection connection = new SqlConnection(connectingStringAddStudent);
+            connection.Open();
+
+            SqlCommand komut = new SqlCommand("Delete from exams where stdNo = @ogrNo", connection);
+            komut.Parameters.Add(new SqlParameter("@ogrNo", ogrNo));
+            komut.ExecuteNonQuery();
+            connection.Close();
+
+        }
+        /// <summary>
+        /// Öğrenci notlarını listeleme işlemi yapar.
+        /// </summary>     
+        public static DataSet noteList()
+    {
+        SqlConnection connection = new SqlConnection(connectingStringAddStudent);
+        SqlDataAdapter adapter = new SqlDataAdapter("SELECT addStudent.stdNo , addStudent.name , addStudent.surname,exams.midterm ,exams.final , exams.result FROM addStudent , exams WHERE exams.stdNo = addStudent.stdNo", connection);
+        DataSet ds = new DataSet();
+        //ds.Clear();
+        adapter.Fill(ds);
+        return ds;
+
+  }
+
+    }
+       }
